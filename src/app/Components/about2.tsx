8345f3e489c545837a474ddb18aa2c76101b3c64
ansh4359 <anshsingh4359@gmail.com>
2025-07-29 15:20:38 +0530

    Changes in the about

--- src/app/Components/about2.tsx
+++ src/app/Components/about2.tsx
@@ -67,7 +67,7 @@ function About2() {
               <div className="space-y-4 text-white">
                 <h2 className="text-xl font-bold font-sans">Our Mission</h2>
                 <p className="text-blue-100 font-sans leading-relaxed text-sm">
-                TaxaGo Consultancy Services  was established in 1 march 2022 By Sanjeev Kumar Singh and his Associates, the firm has grown in strength over the years with plethora of knowledge and experience of its professionals and consultants during last three Year.
+                TaxaGo Consultancy Services  was established on 1st March 2022 By Mr.Sanjeev Kumar Singh and his Associates.Since then, the firm has grown in strength over the years with plethora of knowledge and experience of it's professionals.
                 </p>
 
                 <div className="space-y-3 pt-3">
